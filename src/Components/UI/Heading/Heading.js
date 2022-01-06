4c7529e42023c7db1f2e3170d18dfcf06781e9ea
pranay101 <pranayprajapati101@gmail.com>
2022-01-06 18:31:53 +0530

    added responsiveness on heading part

--- src/Components/UI/Heading/Heading.js
+++ src/Components/UI/Heading/Heading.js
@@ -5,8 +5,8 @@ import classes from "./Heading.module.css";
 const Heading = () => {
   return (
     <div className={classes.Heading}>
-      <h3>Hello there are you looking for </h3>
-      <h1>Career  <span className={classes.pinkBackground}> Opporinuties??</span></h1>
+      <h3>Are you looking for </h3>
+      <h1>Career <span className={classes.pinkBackground}> Opporinuties?? </span></h1>
     </div>
   );
 };
