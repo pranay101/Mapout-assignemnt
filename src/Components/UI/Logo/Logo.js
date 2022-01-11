0ee927f9bf434760c2412fd140873b13da10be5e
pranay101 <pranayprajapati101@gmail.com>
2022-01-11 12:58:12 +0530

    updated package.json

--- src/Components/UI/Logo/Logo.js
+++ src/Components/UI/Logo/Logo.js
@@ -5,7 +5,7 @@ import Classes from "./Logo.module.css"
 
 const Logo = () =>{
     return (
-        <div className={Classes.Logo , Classes.animation}>
+        <div className={[Classes.Logo , Classes.animation].join(' ')}>
             <p>Mapout</p>
         </div>
     )
