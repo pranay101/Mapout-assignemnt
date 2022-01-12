3578fba13351dc68666c6be3bc0febb8ee978c62
pranay101 <pranayprajapati101@gmail.com>
2022-01-12 17:35:05 +0530

    Updates

---
+++ precache-manifest.9d83feb53273e05c8c6f36574f473531.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94fc0243429410fbc6991f0b5aef75b7",
+    "url": "/Mapout-assignemnt/index.html"
+  },
+  {
+    "revision": "a1126e51b58737dac453",
+    "url": "/Mapout-assignemnt/static/css/main.f70c52d0.chunk.css"
+  },
+  {
+    "revision": "bcd60d352792db48242b",
+    "url": "/Mapout-assignemnt/static/js/2.95d09dda.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/Mapout-assignemnt/static/js/2.95d09dda.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0396cad88426f947fe6",
+    "url": "/Mapout-assignemnt/static/js/3.ebeef9ad.chunk.js"
+  },
+  {
+    "revision": "a1126e51b58737dac453",
+    "url": "/Mapout-assignemnt/static/js/main.cc582bc0.chunk.js"
+  },
+  {
+    "revision": "09c83a9955295232f269",
+    "url": "/Mapout-assignemnt/static/js/runtime-main.5883a028.js"
+  }
+]);
